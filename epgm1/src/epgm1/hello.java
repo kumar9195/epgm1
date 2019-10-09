8e094d9ef5d9c9306a49f5d75b46e361205547f8
Nagaraju <Nagaraju@DESKTOP-Q7GR8E7>
2019-10-09 16:17:45 +0530

    print statement added

--- epgm1/src/epgm1/hello.java
+++ epgm1/src/epgm1/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world kumar");
-
+		System.out.println("hello world kumar");
 	}
 
 }
